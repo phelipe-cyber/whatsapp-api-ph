f00778ed09272061e0fe8e09b43ba85dda051eef
phelipesilveira <phelipe.silveira@trocafone.com>
2023-09-15 11:58:08 -0300

    Packege.joson

--- bot.js
+++ bot.js
@@ -263,49 +263,6 @@ app.post('/send-media', [
     }
   });
   
-  client.on('message_create', async (msg) => {
-    // Fired on all message creations, including your own
-    if (msg.fromMe) {
-        if (msg.body === '!'){
-            const GROUP_NAME = 'GrupoTesteBot';
-
-            const chats = await client.getChats();
-            const group = chats.find(chat => chat.isGroup && chat.name === GROUP_NAME);
-            
-            console.log('group: ' + group.name);
-        
-            if (group) {
-                const participants = await group.participants;
-                const contacts = participants.map(participant => participant.id._serialized);
-                console.log(`contacts do grupo ${GROUP_NAME}: ` + contacts);
-        
-                for (const contact of contacts) {
-                    const buttons = [
-                        {
-                            body: 'Gostaria de continuar'
-                        },
-                        {
-                            body: 'Pode me remover'
-                        }
-                    ];
-                    const message = new Buttons(
-                        `Olá! Sou o chatbot do admin do grupo ${GROUP_NAME}, estou entrando em contato pois estamos no inicio do semeste e precisamos limpar o grupo para adicionar os novos alunos... \n\n Ai gostaria de saber se você gostaria de continuar no grupo ${GROUP_NAME}?`,
-                        buttons,
-                        `Questionário sobre o grupo ${GROUP_NAME}`,
-                        ''
-                    );
-        
-                    console.log(`sendMessage to: ${contact}, message: ${message}`);
-                    await client.sendMessage(contact, message);
-                }
-            } else {
-                console.log('Grupo não encontrado');
-            }
-        }
-    }
-});
-
-
 client.on('message', async msg => {
     console.log('MESSAGE RECEIVED', msg);
 
