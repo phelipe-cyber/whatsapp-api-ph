1880c0274ae28bdf77f2eec6a6a493ed3410f61c
phelipe <phelipegeraldo@hotmail.com>
2025-06-20 13:11:51 -0300

    puppeteer

--- public/botzdg.js
+++ public/botzdg.js
@@ -36,18 +36,18 @@ app.get('/', (req, res) => {
 const client = new Client({
   authStrategy: new LocalAuth({ clientId: 'BOT-PH' }),
   
-  // puppeteer: { 
-  //   headless: false,
-  //   args: [
-  //     '--no-sandbox',
-  //     '--disable-setuid-sandbox',
-  //     '--disable-dev-shm-usage',
-  //     '--disable-accelerated-2d-canvas',
-  //     '--no-first-run',
-  //     '--no-zygote',
-  //     '--single-process', // <- this one doesn't works in Windows
-  //     '--disable-gpu'
-  //   ] }
+  puppeteer: { 
+    headless: false,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process', // <- this one doesn't works in Windows
+      '--disable-gpu'
+    ] }
 });
 
 client.initialize();
